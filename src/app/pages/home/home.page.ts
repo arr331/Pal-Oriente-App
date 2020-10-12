333da261a823c19cec5337d8b7dda2d0dc62f502
arr331 <43158042+arr331@users.noreply.github.com>
2020-10-12 14:01:06 -0500

    Página información

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { MunicipalityService } from 'src/app/services/municipality.service';
 
 @Component({
   selector: 'app-home',
