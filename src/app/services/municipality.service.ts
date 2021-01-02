ca157afeb8ab29b45d324682b6ed93acf3651e7d
Carolina Giraldo <carolinagiraldo@saimyr.com.co>
2021-01-02 10:29:13 -0600

    Botones de back en algunas paginas y servicio de municipio

--- src/app/services/municipality.service.ts
+++ src/app/services/municipality.service.ts
@@ -5,22 +5,21 @@ import { AngularFireDatabase } from 'angularfire2/database';
   providedIn: 'root'
 })
 export class MunicipalityService {
-  municipalityList = [];
+  municipalityList = {};
 
   constructor(private db: AngularFireDatabase) { }
 
   async getMunicipios(region: string) {
-    console.log('Estoy en el servicio', this.municipalityList);
-    if (this.municipalityList.length === 0) {
+    if (!this.municipalityList[region]) {
+      this.municipalityList = {... this.municipalityList, [region] : []}
       await this.db.list(`${region}/MUNICIPALITIES`).query.once('value').then(data => {
-        data.forEach(mun => { this.municipalityList.push(mun.val()) });
+        data.forEach(mun => { this.municipalityList[region].push(mun.val()) });
       });
-      console.log(this.municipalityList, 'Recupero desde fireb');  
     }
-    return this.municipalityList;
+    return this.municipalityList[region];
   }
 
-  getMunicipalitiesById(idMun) {
-    return this.municipalityList.filter(mun => mun.idMun == idMun)[0];
+  getMunicipalitiesById(idMun, region) {
+    return this.municipalityList[region].filter(mun => mun.idMun == idMun)[0];
   }
 }
