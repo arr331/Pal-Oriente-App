b4e026463c13d0fb612a60e95ac1e259304efa73
yesid47 <orrego47@gmail.com>
2021-02-13 11:42:19 -0500

    add map to sites

--- src/app/pages/municipalities/site/components/located/located.component.ts
+++ src/app/pages/municipalities/site/components/located/located.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Site } from '../../../../../interfaces/site';
+
+declare var google;
 
 @Component({
   selector: 'app-located',
@@ -7,8 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocatedComponent implements OnInit {
 
-  constructor() { }
+  mapRef = null;
+  @Input() sitio : Site;
 
-  ngOnInit() {}
+  constructor( private loadingCtrl: LoadingController) { }
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  async loadMap(){
+    const loading = await this.loadingCtrl.create();
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapRef = new google.maps.Map(mapEle, {
+      center: {lat: Number.parseFloat(this.sitio.x) , lng: Number.parseFloat(this.sitio.y)},
+      zoom: 12
+    });
+    google.maps.event
+    .addListenerOnce(this.mapRef, 'idle', () => {
+      loading.dismiss();
+      this.addMaker(Number.parseFloat(this.sitio.x), Number.parseFloat(this.sitio.y));
+    });
+  }
+
+  private addMaker(lat: number, lng: number) {
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.mapRef,
+      title: 'Hello World!'
+    });
+  }
 
 }
