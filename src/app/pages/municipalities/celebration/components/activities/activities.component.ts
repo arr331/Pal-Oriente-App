8993c3bbeaf5ea0ad5e251d789e821536b1e3838
yesid47 <orrego47@gmail.com>
2021-05-05 21:57:32 -0500

    add skeleton in municipality

--- src/app/pages/municipalities/celebration/components/activities/activities.component.ts
+++ src/app/pages/municipalities/celebration/components/activities/activities.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
 import { Celebration } from '../../../../../interfaces/celebration';
 import { Activity } from '../../../../../interfaces/activity';
 
