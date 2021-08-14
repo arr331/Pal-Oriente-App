f07742e9c0e80be835a57629c1b2682d13ce9e4c
Carolina <caroliina20003@gmail.com>
2021-08-13 23:06:13 -0500

    Guardias faltantes y guardia de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,73 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './services/login/auth.guard';
+import { AuthGuard, LoginGuard } from './services/login/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule), canActivate: [AuthGuard]
-  },
-  {
-    path: 'municipality-menu',
-    loadChildren: () => import('./pages/municipalities/municipality-menu/municipality-menu.module').then( m => m.MunicipalityMenuPageModule)
-  },
-   {
-     path: 'site',
-     loadChildren: () => import('./pages/municipalities/site/site.module').then( m => m.SitePageModule)
-  },
-  {
-    path: 'celebration',
-    loadChildren: () => import('./pages/municipalities/celebration/celebration.module').then( m => m.CelebrationPageModule)
-  },
-  {
-    path: 'information',
-    loadChildren: () => import('./pages/municipalities/information/information.module').then( m => m.InformationPageModule)
-  },
-  {
-    path: 'site-menu',
-    loadChildren: () => import('./pages/municipalities/site-menu/site-menu.module').then( m => m.SiteMenuPageModule)
-  },
-  {
-    path: 'celebration-menu',
-    loadChildren: () => import('./pages/municipalities/celebration-menu/celebration-menu.module').then( m => m.CelebrationMenuPageModule)
-  },
-  {
-    path: 'menu-new',
-    loadChildren: () => import('./pages/notices/menu-new/menu-new.module').then( m => m.MenuNewPageModule)
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'map',
-    loadChildren: () => import('./pages/map/map.module').then( m => m.MapPageModule)
-  },
-  {
-    path: 'menu-information',
-    loadChildren: () => import('./pages/information/menu-information/menu-information.module').then( m => m.MenuInformationPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canActivate: [LoginGuard]
   },
   {
     path: 'terms',
-    loadChildren: () => import('./components/terms/terms.module').then( m => m.TermsPageModule)
-  },  {
-    path: 'user-profile',
-    loadChildren: () => import('./components/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
+    loadChildren: () => import('./components/terms/terms.module').then( m => m.TermsPageModule),
+    canActivate: [LoginGuard]
   },
-
-
+  {
+    path: 'municipality-menu',
+    loadChildren: () => import('./pages/municipalities/municipality-menu/municipality-menu.module').then( m => m.MunicipalityMenuPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'site',
+    loadChildren: () => import('./pages/municipalities/site/site.module').then( m => m.SitePageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'celebration',
+    loadChildren: () => import('./pages/municipalities/celebration/celebration.module').then( m => m.CelebrationPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'information',
+    loadChildren: () => import('./pages/municipalities/information/information.module').then( m => m.InformationPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'site-menu',
+    loadChildren: () => import('./pages/municipalities/site-menu/site-menu.module').then( m => m.SiteMenuPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'celebration-menu',
+    loadChildren: () => import('./pages/municipalities/celebration-menu/celebration-menu.module').then( m => m.CelebrationMenuPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'menu-information',
+    loadChildren: () => import('./pages/information/menu-information/menu-information.module').then( m => m.MenuInformationPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'menu-new',
+    loadChildren: () => import('./pages/notices/menu-new/menu-new.module').then( m => m.MenuNewPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./pages/map/map.module').then( m => m.MapPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'user-profile',
+    loadChildren: () => import('./components/user-profile/user-profile.module').then( m => m.UserProfilePageModule),
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
--- src/app/services/login/auth.guard.ts
+++ src/app/services/login/auth.guard.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { CanActivate} from '@angular/router';
 import { NavController } from '@ionic/angular';
-import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Storage } from '@ionic/storage';
 
@@ -20,14 +14,7 @@ export class AuthGuard implements CanActivate {
     private authService: AuthService
   ) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  canActivate(): Promise<boolean> {
     return new Promise((res) => {
       this.storage.get('user').then(
         async (resp) => {
@@ -37,10 +24,40 @@ export class AuthGuard implements CanActivate {
             this.navCtrl.navigateForward('/login');
             res(false);
           }
-        },
-        (err) => {
+        }, (err) => {
           this.authService.SignOut();
           res(false);
+          console.error(err);
+        }
+      );
+    });
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(
+    private navCtrl: NavController,
+    private storage: Storage,
+    private authService: AuthService
+  ) {}
+
+  canActivate(): Promise<boolean> {
+    return new Promise((res) => {
+      this.storage.get('user').then(
+        (resp) => {
+          if (resp == null) {
+            res(true);
+          } else {
+            this.navCtrl.navigateForward('/tabs/home');
+            res(false);
+          }
+        }, (err) => {
+          // mensaje de error para volver a loguear
+          this.authService.SignOut();
+          res(true);
           console.error(err);
         }
       );
