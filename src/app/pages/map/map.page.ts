8d05dda9f96fb6e8cc33414b3eb3a1bcdb33b21d
yesid47 <orrego47@gmail.com>
2021-08-16 18:38:36 -0500

    exclude to state

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -17,28 +17,31 @@ export class MapPage implements OnInit {
   constructor(private loadingCtrl: LoadingController, private mapService: MapService) { }
 
 
-  ngOnInit() {
+  public ngOnInit(): void{
     this.mapService.getCordinates().then(data => {
       data.forEach(mun => {
         const coordinates = Object.keys(mun).map(coor => mun[coor]);
         this.coordinatesList.push(...coordinates); 
         this.loadMap();
-      });
+      },);
     });
   }
 
-  async loadMap() {
-    const loading = await this.loadingCtrl.create();
+  public async loadMap() {
+    const loading = await this.loadingCtrl.create({
+      message : 'Espere por favor'
+    });
+    await loading.present();
     const mapEle: HTMLElement = document.getElementById('map');
     this.mapRef = new google.maps.Map(mapEle, {
       center: { lat: 6.1383542, lng: -75.2729218 },
       zoom: 10
     });
     google.maps.event.addListenerOnce(this.mapRef, 'idle', () => {
-      loading.dismiss();
       for (let item of this.coordinatesList){
         this.addMaker(parseFloat(item.x), parseFloat(item.y), item.name);
       }
+      loading.dismiss()
     });
   }
 
